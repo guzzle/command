6ea93543c6fb89fdfe5c4c327b20d70686f02ede
Michael Dowling <mtdowling@gmail.com>
2014-03-21 00:18:39 -0700

    Adding an abstract client implementation

---
+++ src/AbstractClient.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace GuzzleHttp\Command;
+
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Collection;
+use GuzzleHttp\Event\HasEmitterTrait;
+use GuzzleHttp\Command\Exception\CommandException;
+use GuzzleHttp\Command\Event\EventWrapper;
+
+/**
+ * Abstract client implementation that provides a basic implementation of
+ * several methods. Concrete implementations may choose to extend this class
+ * or to completely implement all of the methods of ServiceClientInterface.
+ */
+abstract class AbstractClient implements ServiceClientInterface
+{
+    use HasEmitterTrait;
+
+    /** @var ClientInterface HTTP client used to send requests */
+    private $client;
+
+    /** @var Collection Service client configuration data */
+    private $config;
+
+    /**
+     * The default client constructor is responsible for setting private
+     * properties on the client and accepts an associative array of
+     * configuration parameters:
+     *
+     * - defaults: Associative array of default command parameters to add to
+     *   each command created by the client.
+     *
+     * Concrete implementations may choose to support additional configuration
+     * settings as needed.
+     *
+     * @param ClientInterface $client Client used to send HTTP requests
+     * @param array           $config Client configuration options
+     */
+    public function __construct(
+        ClientInterface $client,
+        array $config = []
+    ) {
+        $this->client = $client;
+        // Ensure the defaults key is an array so we can easily merge later.
+        if (!isset($config['defaults'])) {
+            $config['defaults'] = [];
+        }
+        $this->config = new Collection($config);
+    }
+
+    public function __call($name, array $arguments)
+    {
+        return $this->execute($this->getCommand($name, $arguments[0]));
+    }
+
+    public function execute(CommandInterface $command)
+    {
+        try {
+            $event = EventWrapper::prepareCommand($command, $this);
+            // Listeners can intercept the event and inject a result. If that
+            // happened, then we must not emit further events and just
+            // return the result.
+            if (null !== ($result = $event->getResult())) {
+                return $result;
+            }
+            $request = $event->getRequest();
+            // Send the request and get the response that is used in the
+            // complete event.
+            $response = $this->client->send($request);
+            // Emit the process event for the command and return the result
+            return EventWrapper::processCommand($command, $this, $request, $response);
+        } catch (CommandException $e) {
+            // Let command exceptions pass through untouched
+            throw $e;
+        } catch (\Exception $e) {
+            // Wrap any other exception in a CommandException so that exceptions
+            // thrown from the client are consistent and predictable.
+            $msg = 'Error executing command: ' . $e->getMessage();
+            throw new CommandException($msg, $this, $command, null, null, $e);
+        }
+    }
+
+    public function executeAll($commands, array $options = [])
+    {
+        $requestOptions = [];
+        // Move all of the options over that affect the request transfer
+        if (isset($options['parallel'])) {
+            $requestOptions['parallel'] = $options['parallel'];
+        }
+
+        // Create an iterator that yields requests from commands and send all
+        $this->client->sendAll(
+            new CommandToRequestIterator($commands, $this, $options),
+            $requestOptions
+        );
+    }
+
+    public function getHttpClient()
+    {
+        return $this->client;
+    }
+
+    public function getConfig($keyOrPath = null)
+    {
+        if ($keyOrPath === null) {
+            return $this->config->toArray();
+        }
+
+        if (strpos($keyOrPath, '/') === false) {
+            return $this->config[$keyOrPath];
+        }
+
+        return $this->config->getPath($keyOrPath);
+    }
+
+    public function setConfig($keyOrPath, $value)
+    {
+        $this->config->setPath($keyOrPath, $value);
+    }
+}
---
+++ tests/AbstractClientTest.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace GuzzleHttp\Tests\Command\Guzzle;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Command\Command;
+use GuzzleHttp\Command\Event\EventWrapper;
+use GuzzleHttp\Command\Event\ProcessEvent;
+use GuzzleHttp\Message\Request;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Command\Event\PrepareEvent;
+use GuzzleHttp\Command\Exception\CommandException;
+use GuzzleHttp\Event\BeforeEvent;
+
+/**
+ * @covers \GuzzleHttp\Command\AbstractClient
+ */
+class AbstractClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasConfig()
+    {
+        $client = new Client();
+        $config = [
+            'foo' => 'bar',
+            'baz' => ['bam' => 'boo']
+        ];
+        $sc = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, $config])
+            ->getMockForAbstractClass();
+        $this->assertSame($client, $sc->getHttpClient());
+        $this->assertEquals('bar', $sc->getConfig('foo'));
+        $this->assertEquals('boo', $sc->getConfig('baz/bam'));
+        $this->assertEquals([], $sc->getConfig('defaults'));
+        $sc->setConfig('abc/123', 'listen');
+        $this->assertEquals('listen', $sc->getConfig('abc/123'));
+    }
+
+    public function testMagicMethodExecutesCommands()
+    {
+        $client = new Client();
+        $mock = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, []])
+            ->setMethods(['getCommand', 'execute'])
+            ->getMock();
+
+        $mock->expects($this->once())
+            ->method('getCommand')
+            ->with('foo', [])
+            ->will($this->returnValue(new Command('foo')));
+
+        $mock->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue('foo'));
+
+        $this->assertEquals('foo', $mock->foo([]));
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Command\Exception\CommandException
+     */
+    public function testPassesCommandExceptionsThrough()
+    {
+        $client = new Client();
+        $mock = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, []])
+            ->setMethods(['getCommand'])
+            ->getMock();
+        $command = new Command('foo');
+        $command->getEmitter()->on('prepare', function(PrepareEvent $event) {
+            throw new CommandException(
+                'foo',
+                $event->getClient(),
+                $event->getCommand(),
+                $event->getRequest()
+            );
+        }, 1);
+        $mock->execute($command);
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Command\Exception\CommandException
+     * @expectedExceptionMessage Error executing command: msg
+     */
+    public function testWrapsExceptionsInCommandExceptions()
+    {
+        $client = new Client();
+        $mock = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, []])
+            ->getMockForAbstractClass();
+        $command = new Command('foo');
+        $command->getEmitter()->on('prepare', function(PrepareEvent $event) {
+            throw new \Exception('msg');
+        });
+        $mock->execute($command);
+    }
+
+    public function testReturnsInterceptedResult()
+    {
+        $client = new Client();
+        $mock = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, []])
+            ->getMockForAbstractClass();
+        $command = new Command('foo');
+        $command->getEmitter()->on('prepare', function(PrepareEvent $event) {
+            $event->setResult('test');
+        });
+        $this->assertEquals('test', $mock->execute($command));
+    }
+
+    public function testReturnsProcessedResponse()
+    {
+        $client = new Client();
+        $client->getEmitter()->on('before', function (BeforeEvent $event) {
+            $event->intercept(new Response(201));
+        });
+        $mock = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, []])
+            ->getMockForAbstractClass();
+        $command = new Command('foo');
+        $command->getEmitter()->on('prepare', function(PrepareEvent $e) {
+            $e->setRequest($e->getClient()->getHttpClient()->createRequest(
+                'GET', 'http://test.com')
+            );
+        });
+        $command->getEmitter()->on('process', function(ProcessEvent $e) {
+            $e->setResult('foo');
+        });
+        $this->assertEquals('foo', $mock->execute($command));
+    }
+
+    public function testExecutesCommandsInParallel()
+    {
+        $client = $this->getMockBuilder('GuzzleHttp\\Client')
+            ->setMethods(['sendAll'])
+            ->getMock();
+        $mock = $this->getMockBuilder('GuzzleHttp\\Command\\AbstractClient')
+            ->setConstructorArgs([$client, []])
+            ->getMockForAbstractClass();
+        $command = new Command('foo');
+        $request = $client->createRequest('GET', 'http://httbin.org');
+        $command->getEmitter()->on('prepare', function (PrepareEvent $e) use ($request) {
+            $e->setRequest($request);
+        });
+
+        $client->expects($this->once())
+            ->method('sendAll')
+            ->will($this->returnCallback(function ($requests, $options) use ($request) {
+                $this->assertEquals(10, $options['parallel']);
+                $this->assertTrue($requests->valid());
+                $this->assertSame($request, $requests->current());
+            }));
+
+        $mock->executeAll([$command], ['parallel' => 10]);
+    }
+}
