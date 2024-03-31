de1addc7e64e94e5582d9d6a57ea015233f0b6a1
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2024-03-31 11:09:07 +0100

    Fixes for PHP 8.4 deprecation (#49)

--- src/ServiceClient.php
+++ src/ServiceClient.php
@@ -49,7 +49,7 @@ class ServiceClient implements ServiceClientInterface
         HttpClient $httpClient,
         callable $commandToRequestTransformer,
         callable $responseToResultTransformer,
-        HandlerStack $commandHandlerStack = null
+        ?HandlerStack $commandHandlerStack = null
     ) {
         $this->httpClient = $httpClient;
         $this->commandToRequestTransformer = $commandToRequestTransformer;
